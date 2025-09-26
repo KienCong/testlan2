56f0879f020978d7b5f7dacebf8f42cb78505dec
CongKien <kienlecong205@gmail.com>
2025-09-26 11:13:21 +0700

    sua them loop in ra xin chao

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,8 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         System.out.println("goodbye");
+        for (int i = 0 ; i < 10; i++) {
+            System.out.println("xin chao " + i);
+        }
     }
 }
